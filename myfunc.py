5a5acb5feecd3a5e2a6e611734237c87f00bc436
AlekseyIgnatov <127142323+AlekseyIgnatov@users.noreply.github.com>
2023-06-20 11:54:44 +0300

    Add files via upload
    added homework to the seventh lesson

--- myfunc.py
+++ myfunc.py
@@ -13,3 +13,22 @@ def sum(a,b):
         return a
     else: 
         return sum(a+1,b-1)
+    
+# Функция заполнения массива числами арифметической прогрессии
+def arif_progr(a1,d,n):
+    an = [a1+(i)*d for i in range(n)]
+    return an
+
+# Функция поиска индексов чисел массива попадающих в указанный диапазон
+def index_mas(List, mind, maxd):
+    List_ind=[]
+    for i in range(len(List)):
+        if List[i] >= mind and List[i] <= maxd:
+            List_ind.append(i)
+    return List_ind
+
+# Функция вывода таблицы умножения
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    List = [[(i+1)*(j+1) for i in range(num_rows)] for j in range(num_columns)]
+    for row in List:
+        print (' '.join(map(str, row)))
